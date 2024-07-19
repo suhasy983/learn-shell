75603cd4911f79a2f89c81a45ccaf24ed47f3ca6
suhas yewale <suhasy983@gmail.com>
2024-07-19 12:08:54 +0530

    script for redis

---
+++ function.sh
@@ -0,0 +1,35 @@
+# Function is a name given to a set of commands.
+# Syntax
+
+# function abc() {
+# commands
+# commands
+# }
+
+# OR
+
+# abc() {
+# commands
+# commands
+#}
+
+# How to call a function. - Simply call the function like a command
+
+# abc
+
+
+SAMPLE() {
+  echo SAMPLE FUNCTION
+}
+
+SAMPLE
+SAMPLE
+
+# Variables in functions
+# -->  Declare variable in function, Main program can access it and vice-versa
+
+DEMO() {
+  echo First Argument - $1
+}
+
+DEMO abc
